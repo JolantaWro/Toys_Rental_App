5250d43c0dc27dc83b961cf452820a74f11ca6a4
Mateusz-11 <tweak.night@gmail.com>
2023-08-21 00:12:14 +0200

    Change static code to map function in HowItWorks

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -6,13 +6,28 @@ import imgstep3 from "../../src/assets/img/step3.png";
 import imgstep4 from "../../src/assets/img/step4.png";
 
 const HowItWorks = () => {
+	const steps = [
+		{ id: 1, image1: imgstep1, description: "Choose toys" },
+		{ id: 2, image1: imgstep2, description: "Send the order" },
+		{
+			id: 3,
+			image1: imgstep3,
+			description: "Pick up toys in our store",
+		},
+		{
+			id: 4,
+			image1: imgstep4,
+			description: "Have fun with children",
+		},
+	];
 	return (
 		<div className='holder'>
 			<h2 className='holder__title'>
 				How it <span className='holder__title--highlight'>works?</span>
 			</h2>
 			<div className='howitworks'>
-				<Row>
+				{/* Old version - without map function */}
+				{/* <Row>
 					<Col span={6} className='howitworks__item'>
 						<div className='howitworks__item'>
 							<div
@@ -61,6 +76,23 @@ const HowItWorks = () => {
 							<span>Have fun with children</span>
 						</div>
 					</Col>
+				</Row> */}
+				{/* New version - with map function */}
+				<Row>
+					{steps.map((step) => (
+						<Col span={6} className='howitworks__item' key={step.id}>
+							<div className='howitworks__item'>
+								<div
+									className='howitworks__item howitworks__img'
+									style={{
+										backgroundImage: `url(${step.image1})`,
+									}}></div>
+							</div>
+							<div className='howitworks__item howitworks__description'>
+								<span>{step.description}</span>
+							</div>
+						</Col>
+					))}
 				</Row>
 			</div>
 		</div>
