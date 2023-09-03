4ea648a021cf8ae42b757e504972d2421b30426d
Mateusz-11 <tweak.night@gmail.com>
2023-09-03 22:26:32 +0200

    to delete

--- server/db.js
+++ server/db.js
@@ -1,7 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-
+	host: "localhost",
+	user: "postgres",
+	password: "1post1gres1",
+	port: 5432,
+	database: "dbtoys",
 });
 
 module.exports = pool;
