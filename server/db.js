2766628ee2e44ba0d4fe6eb5cc3a8f4ec7fedfe2
JolantaWro <jolanta.woronowska@gmail.com>
2023-09-09 14:53:40 +0200

    add config

--- server/db.js
+++ server/db.js
@@ -1,11 +1,22 @@
 const Pool = require("pg").Pool;
+// const {Pool} = require("pg")
+
+
+// const pool = new Pool({
+// 	host: "localhost",
+// 	user: "postgres",
+// 	password: "",
+// 	port: 5432,
+// 	database: "dbtoys",
+// });
 
 const pool = new Pool({
-	host: "localhost",
-	user: "postgres",
-	password: "",
-	port: 5432,
-	database: "dbtoys",
-});
+	connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  })
+
+pool.connect((err) => {
+	if (err) throw err
+	console.log("connect successfully")
+})
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+require('dotenv').config()
+
 //middleware
 
 app.use(cors());
@@ -12,6 +14,8 @@ app.use("/auth", require("./routes/auth"));
 app.use("/dashboard", require("./routes/dashboard"));
 
 
-app.listen(5000, () => {
-  console.log(`Server is starting on port 5000`);
-});
+// app.listen(5000, () => {
+//   console.log(`Server is starting on port 5000`);
+// });
+
+app.listen(process.env.PORT, () => console.log("server is running"))
