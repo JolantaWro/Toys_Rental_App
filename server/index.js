3ed2978f1171bda3b8b58845c48b34a49f9b4f3e
JolantaWro <jolanta.woronowska@gmail.com>
2023-09-14 10:32:08 +0200

    test conection

--- server/index.js
+++ server/index.js
@@ -7,15 +7,15 @@ require('dotenv').config()
 //middleware
 
 app.use(cors());
-app.use(express.json()); //req.body
+app.use(express.json()); //req.bodygit 
 
 //routes
 app.use("/auth", require("./routes/auth"));
 app.use("/dashboard", require("./routes/dashboard"));
 
 
-// app.listen(5000, () => {
-//   console.log(`Server is starting on port 5000`);
-// });
+app.listen(5000, () => {
+  console.log(`Server is starting on port 5000`);
+});
 
-app.listen(process.env.PORT, () => console.log("server is running"))
+// app.listen(process.env.PORT, () => console.log("server is running"))
