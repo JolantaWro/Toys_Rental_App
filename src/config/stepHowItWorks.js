2d58dd203fd6dff4ead996b906138e4e7ba4e519
Mateusz-11 <tweak.night@gmail.com>
2023-09-17 13:22:28 +0200

    Style of login form - finish

--- src/config/stepHowItWorks.js
+++ src/config/stepHowItWorks.js
@@ -1,19 +1,19 @@
-import imgstep1 from "../assets/img/step1.png";
-import imgstep2 from "../assets/img/step2.png";
-import imgstep3 from "../assets/img/step3.png";
-import imgstep4 from "../assets/img/step4.png";
+import imgcategory1 from "../assets/img/step1.png";
+import imgcategory2 from "../assets/img/step2.png";
+import imgcategory3 from "../assets/img/step3.png";
+import imgcategory4 from "../assets/img/step4.png";
 
 export const steps = [
-	{ id: 1, image: imgstep1, description: "Choose toys" },
-	{ id: 2, image: imgstep2, description: "Send the order" },
+	{ id: 1, image: imgcategory1, description: "Choose toys" },
+	{ id: 2, image: imgcategory2, description: "Send the order" },
 	{
 		id: 3,
-		image: imgstep3,
+		image: imgcategory3,
 		description: "Pick up toys in our store",
 	},
 	{
 		id: 4,
-		image: imgstep4,
+		image: imgcategory4,
 		description: "Have fun with children",
 	},
 ];
