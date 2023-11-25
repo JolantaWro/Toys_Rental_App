6aadf85173577e31a1f776f3493cc4a01289aa36
JolantaWro <jolanta.woronowska@gmail.com>
2023-11-25 21:12:04 +0100

    change path in rental

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -10,7 +10,7 @@ const Rental = () => {
 
 	const getProduct = async () => {
 		try {
-			const res = await fetch("http://toys-rental-app.vercel.app/rental/");
+			const res = await fetch("https://toys-rental-app.vercel.app/rental/");
 			// const res = await fetch("http://localhost:5000/rental/");
 
 			const parseData = await res.json();
