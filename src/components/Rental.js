3624ba7f9b3172e3dcb026d2a415b10b7365a392
JolantaWro <jolanta.woronowska@gmail.com>
2023-11-25 23:18:38 +0100

    change path in rental

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -10,7 +10,8 @@ const Rental = () => {
 
 	const getProduct = async () => {
 		try {
-			const res = await fetch("http://localhost:5000/rental/");
+			const res = await fetch(`${process.env.REACT_APP_API_URL}/rental/`);
+			// const res = await fetch("http://localhost:5000/rental/");
 
 			const parseData = await res.json();
 			setToysProduct(parseData)
