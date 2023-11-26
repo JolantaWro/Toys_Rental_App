5c20ef971ecafea7c0a453f64d6ecab650052f75
JolantaWro <jolanta.woronowska@gmail.com>
2023-11-26 20:17:26 +0100

    change rental path

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -11,8 +11,8 @@ const Rental = () => {
 	const getProduct = async () => {
 		try {
 			console.log(JSON.stringify(process.env))
-			const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_VERCEL_URL : 'http://localhost:5000/rental/';
-			const res = await fetch(`${apiUrl}/rental/`);
+			const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_VERCEL_URL : 'http://localhost:5000';
+			const res = await fetch(`${apiUrl}/rental`);
 			// const res = await fetch(`${apiUrl}`);
 
 
